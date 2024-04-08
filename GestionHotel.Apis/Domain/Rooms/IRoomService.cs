98ef6afff91ca5537c69323626f3601ee5ef0967
yso <baptisteguillaume5@gmail.com>
2024-04-08 22:43:56 +0200

    HotelManagement API rename .sln

--- GestionHotel.Apis/Domain/Rooms/IRoomService.cs
+++ GestionHotel.Apis/Domain/Rooms/IRoomService.cs
@@ -2,5 +2,9 @@
 {
 	public interface IRoomService
 	{
+		Task<List<Room>> GetAvailableRooms(DateTime startDate, DateTime endDate);
+		Task<Room> GetRoomById(int id);
+		Task<Room> BookRoom(int roomId, int clientId, DateTime startDate, DateTime endDate, string paymentMethod);
+		Task<bool> CancelReservation(int reservationId);
 	}
 }
--- GestionHotel.Apis/Services/Room/RoomService.cs
+++ GestionHotel.Apis/Services/Room/RoomService.cs
@@ -1,6 +1,27 @@
-﻿namespace GestionHotel.Apis.Services.Room
+﻿using GestionHotel.Apis.Domain.Rooms;
+
+namespace GestionHotel.Apis.Services.Room
 {
-	public class RoomService
+	public class RoomService : IRoomService
 	{
+		public Task<Domain.Rooms.Room> BookRoom(int roomId, int clientId, DateTime startDate, DateTime endDate, string paymentMethod)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> CancelReservation(int reservationId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Domain.Rooms.Room>> GetAvailableRooms(DateTime startDate, DateTime endDate)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Domain.Rooms.Room> GetRoomById(int id)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
